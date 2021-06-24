84687f4dc61358b47b90721b7d8d535c2d827f90
Ivan Trubić <itrubic.truba@gmail.com>
2021-06-24 13:07:21 +0200

    solidna mreza

--- code/data_setup.py
+++ code/data_setup.py
@@ -2,11 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import random
-import tensorflow as tf
-
-#tf.compat.v1.disable_eager_execution()
-tf.autograph.set_verbosity(0)
-
 
 # Odabir stupaca - IMU senzori sa desne nadkoljenice i potkoljenice
 # Stupci   |rsacc|  |rsgyro|    |-rtacc-|  |-rtgyro-|
@@ -14,11 +9,30 @@ columns = (6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)
 
 actions = ["bicycling", "running", "sitting_in_car", "sitting", "standing", "walking"]
 
-max_len = 0
-final_len = 21155
+max_len = 21200
+final_len = max_len
+g = 9.81
 
 def load_file(path_to_file):
     return np.genfromtxt(path_to_file, delimiter='\t', skip_header=4, usecols=columns, dtype=np.float32)
+
+def normalize(data):
+    normal = np.array([
+        data[0] / (2*g*1000),
+        data[1] / (2*g*1000),
+        data[2] / (2*g*1000),
+        data[3] / 2000000,
+        data[4] / 2000000,
+        data[5] / 2000000,
+        data[6] / (2*g*1000),
+        data[7] / (2*g*1000),
+        data[8] / (2*g*1000),
+        data[9] / 2000000,
+        data[10] / 2000000,
+        data[11] / 2000000,
+    ])
+    return normal
+
 
 def load_data(path):
     x = []
@@ -26,15 +40,16 @@ def load_data(path):
     data = os.listdir(path)
     random.shuffle(data)
     for sample in data:
-        x.append(np.transpose(load_file(path+sample)))
+        for split in np.array_split(pad_data(np.transpose(normalize(load_file(path+sample)))), 4, axis=1):
+            x.append(split)
         for action in actions:
             if action in sample:
                 out = [0.0 for i in actions]
                 out[actions.index(action)] = 1.0
-                y.append(np.transpose(np.array(out, dtype=np.float32)))
+                for j in range(4):
+                    y.append(np.transpose(np.array(out, dtype=np.float32)))
                 break
 
-    print("returning data")
     return x, y
 
 def find_max_len(data_arr):
@@ -50,35 +65,35 @@ def match_len(data_arr):
         find_max_len(data_arr)
 
     for i in range(len(data_arr)):
-        data_arr[i] = np.pad(data_arr[i], ((0, 0), (0, max_len - data_arr[i].shape[1])), mode='constant')
+        data_arr[i] = np.pad(data_arr[i], ((0, 0), (0, max_len - data_arr[i].shape[1])), mode='wrap')
         
     return data_arr
 
+def pad_data(data):
+    return  np.pad(data, ((0, 0), (0, max_len - data.shape[1])), mode='wrap')
 
 db_dir = "/home/truba/Documents/Faks/Diplomski/db/HuGaDB/Data/"
 test_dir = "/home/truba/Documents/Faks/Diplomski/db/HuGaDB/Test/"
 
-#print("setting up training data...")
+print("setting up training data...")
 x_train, y_train = load_data(db_dir)
 print(len(x_train), len(y_train))
-x_train = match_len(x_train)
-print(x_train[0].shape)
+print(x_train[0].shape, y_train[0].shape)
 
-#print("setting up test data...")
+print("setting up test data...")
 x_test, y_test = load_data(test_dir)
 print(len(x_test), len(y_test))
-x_test = match_len(x_test)
 
 
-#print(x_train[0].shape)
 y_train = np.array(y_train, dtype=np.float32)
 x_train = np.array(x_train, dtype=np.float32)
 x_test = np.array(x_test, dtype=np.float32)
 y_test = np.array(y_test, dtype=np.float32)
+print(x_train.shape, y_train.shape)
 
-np.save('x_train.npy', x_train)
-np.save("y_train.npy", y_train)
-np.save("x_test.npy", x_test)
-np.save("y_test.npy", y_test)
+np.save('x_train_normal.npy', x_train)
+np.save("y_train_normal.npy", y_train)
+np.save("x_test_normal.npy", x_test)
+np.save("y_test_normal.npy", y_test)
 print(os.path.abspath("."))
 print("saved data")
